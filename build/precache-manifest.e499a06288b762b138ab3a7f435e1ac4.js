a76b1f92a1f5e54065d666d088e5137f508ab826
flower <1309571292@qq.com>
2018-11-20 17:45:18 +0800

    add build file

---
+++ build/precache-manifest.e499a06288b762b138ab3a7f435e1ac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "194bdfcc1b36f70c643b",
+    "url": "/static/js/main.194bdfcc.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "194bdfcc1b36f70c643b",
+    "url": "/static/css/main.b72b3344.chunk.css"
+  },
+  {
+    "revision": "07d06506dbe1900637c298ad75cd6aa3",
+    "url": "/index.html"
+  }
+];
